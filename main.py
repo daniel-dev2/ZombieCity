3e70b6d2c3ded671fd73b8b4cedc96548bc25a97
Daniel <danielrodrigo098@gmail.com>
2024-05-26 14:38:53 -0300

    added enemy sprites, spawn logic, and movement logi

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 
@@ -11,6 +12,7 @@ pygame.display.set_caption("ZombieCity")
 player_sprite = pygame.image.load("assets/sprites/player_character.png")
 coin_sprite = pygame.image.load("assets/sprites/coin.png")
 bullet_sprite = pygame.image.load("assets/sprites/bullet.png")
+zombie_sprite = pygame.image.load("assets/sprites/zombie.png")
 background_image = pygame.image.load("assets/sprites/city_background.png")
 icon = pygame.image.load("assets/sprites/icon.png")
 pygame.display.set_icon(icon)
@@ -26,6 +28,9 @@ player_sprite_rotated = pygame.transform.rotate(player_sprite_scaled, 5)
 # flipping the sprites
 player_sprite_flipped = pygame.transform.flip(player_sprite_scaled, True, False)
 player_sprite_unflipped = pygame.transform.flip(player_sprite_scaled, False, False)
+
+zombie_sprite_flipped = pygame.transform.flip(zombie_sprite, True, False)
+zombie_sprite_unflipped = pygame.transform.flip(zombie_sprite, False, False)
 
 # color configuration
 BLACK = (0, 0, 0)
@@ -85,11 +90,88 @@ def move(pos, direction, speed, rect):
         rect.centery = pos.y
 
 
+zombie_rect = zombie_sprite.get_rect()
+zombie_direction = pygame.math.Vector2()
+zombie_position = pygame.math.Vector2(zombie_rect.center)
+zombie_speed = 0.5
+zombie_facing_left = False
+zombie_facing_right = False
+
+
+# zombie spawning logic
+def spawn_zombie(pos, rect):
+    LEFT_BORDER_X = (-20, 0)
+    LEFT_BORDER_Y = (124, 630)
+
+    BOTTOM_BORDER_X = (0, 800)
+    BOTTOM_BORDER_Y = (630, 650)
+
+    RIGHT_BORDER_X = (830, 850)
+    RIGHT_BORDER_Y = (124, 630)
+
+    spawn_area = random.randint(0, 2)
+
+    # spawn in left border
+    if spawn_area == 0:
+        pos.x = random.randint(LEFT_BORDER_X[0], LEFT_BORDER_X[1])
+        rect.centerx = pos.x
+
+        pos.y = random.randint(LEFT_BORDER_Y[0], LEFT_BORDER_Y[1])
+        rect.centery = pos.y
+
+    # spawn in bottom border
+    elif spawn_area == 1:
+        pos.x = random.randint(BOTTOM_BORDER_X[0], BOTTOM_BORDER_X[1])
+        rect.centerx = pos.x
+
+        pos.y = random.randint(BOTTOM_BORDER_Y[0], BOTTOM_BORDER_Y[1])
+        rect.centery = pos.y
+
+    # spawn in right border
+    else:
+        pos.x = random.randint(RIGHT_BORDER_X[0], RIGHT_BORDER_X[1])
+        rect.centerx = pos.x
+
+        pos.y = random.randint(RIGHT_BORDER_Y[0], RIGHT_BORDER_Y[1])
+        rect.centery = pos.y
+
+
+spawn_zombie(zombie_position, zombie_rect)
+
+
+# zombie movement logic
+def move_zombie(zombie_pos, zombie_rect, zombie_speed, zombie_direction, player_pos):
+    if zombie_direction.magnitude() > 0:
+        zombie_direction = zombie_direction.normalize()
+    # horizontal movement
+    if zombie_pos.x > player_pos.x:
+        zombie_direction.x = 1
+        zombie_pos.x -= zombie_direction.x * zombie_speed
+        zombie_rect.centerx = zombie_pos.x
+
+    elif zombie_pos.x < player_pos.x:
+        zombie_direction.x = 1
+        zombie_pos.x += zombie_direction.x * zombie_speed
+        zombie_rect.centerx = zombie_pos.x
+
+    # vertical movement
+    if zombie_pos.y > player_pos.y:
+        zombie_direction.y = -1
+        zombie_pos.y += zombie_direction.y * zombie_speed
+        zombie_rect.centery = zombie_pos.y
+
+    elif zombie_pos.y < player_pos.y:
+        zombie_direction.y = 1
+        zombie_pos.y += zombie_direction.y * zombie_speed
+        zombie_rect.centery = zombie_pos.y
+
+
 # main game loop
 game_running = True
 
-while (game_running):
+while game_running:
     window.blit(background_image, (0, 0))
+    move_zombie(zombie_position, zombie_rect, zombie_speed, zombie_direction, player_position)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_running = False
@@ -133,6 +215,7 @@ while (game_running):
         player_sprite_scaled = player_sprite_unflipped
         player_facing_right = False
 
+    window.blit(zombie_sprite, zombie_position)
     window.blit(player_sprite_scaled, player_position)
     pygame.display.flip()
 
