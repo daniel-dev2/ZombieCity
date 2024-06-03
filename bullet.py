0535c11175658cd1d7c551901fd9f9a623c332c6
Daniel <danielrodrigo098@gmail.com>
2024-06-02 23:19:05 -0300

    added more code to bullet class

--- bullet.py
+++ bullet.py
@@ -1,6 +1,13 @@
 import pygame
+from player import Player
 
 
 class Bullet:
     def __init__(self):
         self.sprite = pygame.image.load("assets/sprites/bullet.png")
+        self.sprite = pygame.transform.scale(self.sprite, (16, 16))
+        self.sprite_flipped = pygame.transform.flip(self.sprite, True, False)
+        self.sprite_unflipped = pygame.transform.flip(self.sprite, False, False)
+        self.rect = self.sprite.get_rect()
+        self.pos = pygame.math.Vector2(self.rect.center)
+        # TODO implement bullet class
